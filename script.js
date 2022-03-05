935c678b4afac39a948787e3d587f95a7b1bd3e1
Leonardo Lima Coelho <ryszol@protonmail.com>
2022-03-05 11:57:52 -0400

    Displaying month and year, and buttons back and next.

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 const calendar = document.querySelector(".calendar");
 
-//const date = new Date();
-const date = new Date("06-08-2021");
+const date = new Date();
+//const date = new Date("06-08-2021");
 
 const weeks = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
+const months = [
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro",
+];
+
+// Cria e exibe a div que mostra o mês e ano
+const displayMonth = document.createElement("div");
+displayMonth.classList.add("display-month");
+
+const buttonBackMonth = document.createElement("button");
+buttonBackMonth.classList.add("button-back-month");
+buttonBackMonth.innerHTML = "<";
+
+const textMonth = document.createElement("span");
+textMonth.classList.add("text-month");
+
+const buttonNextMonth = document.createElement("button");
+buttonNextMonth.classList.add("button-next-month");
+buttonNextMonth.innerHTML = ">";
+
+displayMonth.appendChild(buttonBackMonth);
+displayMonth.appendChild(textMonth);
+displayMonth.appendChild(buttonNextMonth);
+
+calendar.appendChild(displayMonth);
 
 const weeksDiv = document.createElement("div");
 weeksDiv.classList.add("weeks");
@@ -26,9 +61,10 @@ calendar.appendChild(weeksDiv);
 let arrayMonth = [];
 
 // Cria variável auxiliar para manipular a data
-console.log("s1", date.getMonth());
 let date2 = date;
-console.log("s", date2.getMonth());
+
+textMonth.innerHTML = `${months[date2.getMonth()]} ${date2.getFullYear()}`;
+
 // pega o último dia do mes atual
 // E guarda em lastDay
 date2.setMonth(date2.getMonth() + 1);
@@ -38,7 +74,7 @@ const lastDay = new Date(date2.setDate(date2.getDate() - 1));
 
 // Define o dia para o primeiro do mês
 date2.setDate(1);
-
+console.log("date2", date2);
 const handlePopulateArrayDate = (arrayMonth, currentDay) => {
   // adiciona o dia com mês e semana no array
   arrayMonth.push({
@@ -53,29 +89,20 @@ const handlePopulateArrayDate = (arrayMonth, currentDay) => {
 
 // verifica se o primeiro dia é no meio da semana
 if (date2.getDay() + 1 > date2.getDate()) {
-  console.log("3");
-
   // define o dia para o primeiro dia dessa semana
   date2.setDate(date2.getDate() - date2.getDay());
 }
 let currentDay = date2;
-console.log("4");
-
-console.log(currentDay <= lastDay);
-console.log("5");
 
 while (currentDay <= lastDay) {
   handlePopulateArrayDate(arrayMonth, currentDay);
 }
 
-console.log(currentDay.getDay());
 if (currentDay.getDay() !== 0) {
   for (let i = currentDay.getDay(); i <= 6; i++) {
     handlePopulateArrayDate(arrayMonth, currentDay);
   }
 }
-
-console.log("array ", arrayMonth);
 
 const days = document.createElement("div");
 days.classList.add("days");
